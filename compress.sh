983169e016f432dfec6df4490d3d2256038d0bbb
Owen Coutts <owen@owencoutts.com>
2012-04-04 10:55:08 -0700

    Use uglifyjs
    Added package.json so that people can just run `npm install` to grab all
    the dependencies.

--- compress.sh
+++ compress.sh
@@ -4,6 +4,6 @@ RESOURCE_FOLDER="./media/"
 
 mkdir -p ${RESOURCE_FOLDER}
 lessc src/style.less --yui-compress > ${RESOURCE_FOLDER}socialshare.min.css
-java -jar lib/yuicompressor-2.4.7.jar src/socialshare.js > ${RESOURCE_FOLDER}socialshare.min.js
+uglifyjs src/socialshare.js > ${RESOURCE_FOLDER}socialshare.min.js
 cp -r src/img/ ${RESOURCE_FOLDER}img/
 cp src/OpenSans-Light.ttf ${RESOURCE_FOLDER}
